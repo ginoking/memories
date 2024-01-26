66ec6c44f66300bce0b485237e1767c58189b9ff
ginoking <gt10245@greattree.com.tw>
2024-01-26 10:36:12 +0800

    test api with url

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: window.location.origin + "/api",
+  baseURL: (import.meta.env.VITE_SOME_KEY) ?? window.location.origin + "/api",
   headers: {
     'Content-Type': 'application/json',
   },
