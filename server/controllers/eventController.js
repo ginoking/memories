696d8f103d7e14e7d6fd36b049d1f37b02cfba30
金沅禹 <gt10245@greattree.com.tw>
2024-02-01 15:02:43 +0800

    移掉註解

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-// const Stories = require("../database/mongodb");
+const Stories = require("../database/mongodb");
 
 const { Storage } = require('@google-cloud/storage');
 
@@ -15,35 +15,28 @@ exports.index = async (req, res) => {
 			for (const bucket of buckets) {
 				console.log(`- ${bucket.name}`);
 			}
-
-			res.json(buckets);
-			return;
 		}
 
-		// console.log();
+		const containEventDays = [];
+		const currentDate = req.body.time;
 
-		res.json(process.env);
+		const emptyArray = Array.from({ length: moment(currentDate).day() }, () => "");
+		const numberArray = Array.from({ length: moment(currentDate).daysInMonth() }, (_, i) => (i + 1).toString());
+		for (const number of emptyArray.concat(numberArray)) {
+			const dateString = `${currentDate}-${(number ?? '').padStart(2, '0')}`;
+			const event = (number !== '') ? await Stories.findOne({
+				date: new Date(dateString)
+			}) : null;
 
-		// const containEventDays = [];
-		// const currentDate = req.body.time;
+			containEventDays.push({
+				date: dateString,
+				number: number,
+				event: event || null
+			});
 
-		// const emptyArray = Array.from({ length: moment(currentDate).day() }, () => "");
-		// const numberArray = Array.from({ length: moment(currentDate).daysInMonth() }, (_, i) => (i + 1).toString());
-		// for (const number of emptyArray.concat(numberArray)) {
-		// 	const dateString = `${currentDate}-${(number ?? '').padStart(2, '0')}`;
-		// 	const event = (number !== '') ? await Stories.findOne({
-		// 		date: new Date(dateString)
-		// 	}) : null;
+		}
 
-		// 	containEventDays.push({
-		// 		date: dateString,
-		// 		number: number,
-		// 		event: event || null
-		// 	});
-
-		// }
-
-		// res.json(containEventDays);
+		res.json(containEventDays);
 	} catch (err) {
 		//如果資料庫出現錯誤時回報 status:500 並回傳錯誤訊息 
 		res.status(500).json({ message: err.message })
