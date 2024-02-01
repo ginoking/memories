27c5c49822cede804d717d74806c034011c755c8
金沅禹 <gt10245@greattree.com.tw>
2024-02-01 14:54:28 +0800

    test secret manager to cloud run env

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,40 +1,40 @@
-// //Import the mongoose module
-// var mongoose = require("mongoose");
+//Import the mongoose module
+var mongoose = require("mongoose");
 
-// //Define a schema
-// var Schema = mongoose.Schema;
+//Define a schema
+var Schema = mongoose.Schema;
 
-// var StoriesSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: [true]
-//   },
-//   date: {
-//     type: Date,
-//     required: [true]
-//   },
-//   des: {
-//     type: String,
-//     required: [true]
-//   },
-//   image: {
-//     type: String,
-//     required: [true]
-//   }
-// });
+var StoriesSchema = new Schema({
+    name: {
+        type: String,
+        required: [true]
+    },
+    date: {
+        type: Date,
+        required: [true]
+    },
+    des: {
+        type: String,
+        required: [true]
+    },
+    image: {
+        type: String,
+        required: [true]
+    }
+});
 
-// // Compile model from schema
-// var Stories = mongoose.model("Stories", StoriesSchema);
+// Compile model from schema
+var Stories = mongoose.model("Stories", StoriesSchema);
 
-// //Set up default mongoose connection
-// var mongoDB = process.env.MONGODB_URI + "/memories";
-// mongoose.connect(mongoDB);
-// // Get Mongoose to use the global promise library
-// mongoose.Promise = global.Promise;
-// //Get the default connection
-// var database = mongoose.connection;
+//Set up default mongoose connection
+var mongoDB = process.env.mongoDBUrI + "/memories";
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var database = mongoose.connection;
 
-// //Bind connection to error event (to get notification of connection errors)
-// database.on("error", console.error.bind(console, "MongoDB connection error:"));
+//Bind connection to error event (to get notification of connection errors)
+database.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// module.exports = Stories;
+module.exports = Stories;
