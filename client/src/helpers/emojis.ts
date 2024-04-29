d1f148ac77acfa2ee60f8d5a5738168a8f6001df
金沅禹 <gt10245@greattree.com.tw>
2024-04-29 14:16:25 +0800

    feat: add new emoji > use emojis.ts to manager emojis

--- client/src/helpers/emojis.ts
+++ client/src/helpers/emojis.ts
@@ -1,6 +1,30 @@
 export default {
-    happy: "&#128518;",
-    love: "&#128525;",
-    sad: "&#128546;",
-    angry: "&#128545;"
+    happy: {
+        key: "happy",
+        text: "😆happy"
+    },
+    love: {
+        key: "love",
+        text: "❤️love"
+    },
+    sad: {
+        key: "sad",
+        text: "😢sad"
+    },
+    angry: {
+        key: "angry",
+        text: "😡angry"
+    },
+    confused: {
+        key: "confused",
+        text: "😕confused"
+    },
+    good: {
+        key: "good",
+        text: "👍good"
+    },
+    bad: {
+        key: "👎bad",
+        text: "👎bad"
+    },
 };
